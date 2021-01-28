4589444cd57570edbce54c98d7c30b3b5292c260
Veselin <vesselin60@gmail.com>
2021-01-28 18:39:20 +0200

    installed locale

--- App.js
+++ App.js
@@ -1,8 +1,36 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
-
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import KeepAwake from 'react-native-keep-awake';
 import moment from 'moment';
+import 'moment/locale/bg';
+
+const App = () => {
+  const [today, setDate] = React.useState(moment()); // Save the current date to be able to trigger an update
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      // Creates an interval which will update the current data every minute
+      // This will trigger a rerender every component that uses the useDate hook.
+      setDate(moment());
+    }, 100);
+    return () => {
+      clearInterval(timer); // Return a funtion to clear the timer so that it will stop being called on unmount
+    };
+  }, []);
+
+  const date = today.format('DD MMMM YYYY');
+  const day = today.format('dddd');
+  const time = today.format('HH:mm:ss');
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.timeText}>{time}</Text>
+      <Text style={styles.dayText}>{day}</Text>
+      <Text style={styles.dateText}>{date}</Text>
+      <KeepAwake />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -15,37 +43,14 @@ const styles = StyleSheet.create({
     color: 'yellowgreen',
     fontSize: 60,
   },
+  dayText: {
+    color: 'yellowgreen',
+    fontSize: 40,
+  },
   dateText: {
     color: 'yellowgreen',
     fontSize: 40,
   },
 });
 
-export default class App extends Component {
-  constructor(...args) {
-    super(...args);
-    this.state = {
-      time: moment().format('LTS'),
-      date: moment().format('LL'),
-    };
-  }
-
-  render() {
-    setTimeout(() => {
-      this.setState({
-        time: moment().format('LTS'),
-        date: moment().format('LL'),
-      });
-    }, 1000);
-
-    return (
-      <View style={styles.container}>
-        <StatusBar style={{ backgroundColor: 'transparent' }} />
-        <Text style={styles.timeText}>{this.state.time}</Text>
-        <Text style={styles.dateText}>{this.state.date}</Text>
-
-        <KeepAwake />
-      </View>
-    );
-  }
-}
+export default App;
---
+++ AppC.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+
+import KeepAwake from 'react-native-keep-awake';
+import moment from 'moment';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  timeText: {
+    color: 'yellowgreen',
+    fontSize: 60,
+  },
+  dateText: {
+    color: 'yellowgreen',
+    fontSize: 40,
+  },
+});
+
+export default class AppC extends Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      time: moment().format('LTS'),
+      date: moment().format('LL'),
+    };
+  }
+
+  render() {
+    setTimeout(() => {
+      this.setState({
+        time: moment().format('LTS'),
+        date: moment().format('LL'),
+      });
+    }, 1000);
+
+    return (
+      <View style={styles.container}>
+        <StatusBar style={{ backgroundColor: 'transparent' }} />
+        <Text style={styles.timeText}>{this.state.time}</Text>
+        <Text style={styles.dateText}>{this.state.date}</Text>
+
+        <KeepAwake />
+      </View>
+    );
+  }
+}
